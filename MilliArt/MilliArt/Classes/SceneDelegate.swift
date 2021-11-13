9f7003bf04e033326393bbf4401a3e20567cf620
Ivan Gorshkov <ivanagorshkov160@gmail.com>
2021-11-13 17:50:03 +0300

    total rm main.storyboard

--- MilliArt/MilliArt/Classes/SceneDelegate.swift
+++ MilliArt/MilliArt/Classes/SceneDelegate.swift
@@ -15,11 +15,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-        guard (scene as? UIWindowScene) != nil else { return }
-
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         let tabBarContainer = TabBarContainer.assemble(with: TabBarContext())
-        window?.rootViewController = tabBarContainer.viewController
-        window?.makeKeyAndVisible()
+      
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = tabBarContainer.viewController // Your initial view controller.
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
